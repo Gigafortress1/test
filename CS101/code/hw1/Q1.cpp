7a2e94afa86fc024f18ce964bbf6cffa072365b8
xubowen <374377504@qq.com>
2021-10-11 10:19:26 +0800

    test

--- CS101/code/hw1/Q1.cpp
+++ CS101/code/hw1/Q1.cpp
@@ -3,30 +3,76 @@
 using namespace std;
 #define tower_number 2
 
-int distance(int x1, int y1, int x2, int y2){
-    return pow((x1-x2),2)+pow((y1-y2),2);
+struct point
+{
+    int x,y;
+};
+struct max_dis
+{
+    int r0,r1;
+};
+
+int dis(point p, point q){
+    return pow((p.x-q.x),2)+pow((p.y-q.y),2);
 }
 
 int main(){
-    int tower[tower_number][2];
+    point tower[tower_number];
     int radius[tower_number];
+    int r_test[tower_number][tower_number];
+    max_dis m_d[tower_number];
+    int S[2] = {0,0};
     for(int i=0;i < tower_number;i++){
-        for(int j=0;j < 2;j++){
-            cin >> tower[i][j];
-        }
+        cin >> tower[i].x >> tower[i].y;
         radius[i] = 0;
+        m_d[i].r0 = 0;
+        m_d[i].r1 = 0;
     }
-    
+
     int end_num;
     cin >> end_num;
-    int end[2];
+    point end;
     for(int i=0;i < end_num;i++){
-        cin >> end[0] >> end[1];
-        if(distance(tower[0][0],tower[0][1],end[0],end[1]) > distance(tower[1][0],tower[1][1],end[0],end[1])){
-            radius[1] = distance(tower[1][0],tower[1][1],end[0],end[1]);
+        cin >> end.x >> end.y;
+        if (dis(tower[0],end)>m_d[0].r0)
+        {
+            m_d[0].r0 = dis(tower[0],end);
+            m_d[0].r1 = dis(tower[1],end);
         }
-        else{
-            radius[0] = distance(tower[0][0],tower[0][1],end[0],end[1]);
+        if (dis(tower[1],end)>m_d[1].r1)
+        {
+            m_d[1].r1 = dis(tower[1],end);
+            m_d[1].r0 = dis(tower[0],end);
+        }
+        
+        if(dis(tower[0],end)>radius[0] && dis(tower[1],end)>radius[1]){
+            r_test[0][0] = dis(tower[0],end);
+            if(m_d[1].r0 > r_test[0][0]){
+                r_test[0][1] = m_d[1].r1;
+            }
+            else{
+                r_test[0][1] = 0;
+            }
+            S[0] = r_test[0][0]+r_test[0][1];
+
+            r_test[1][1] = dis(tower[1],end);
+            if(m_d[0].r1 > r_test[1][1]){
+                r_test[1][0] = m_d[0].r0;                
+            }
+            else{
+                r_test[1][0] = 0;
+            }
+            S[1] = r_test[1][0]+r_test[1][1];
+
+            if (S[0]<S[1])
+            {
+                radius[0] = r_test[0][0];
+                radius[1] = r_test[0][1];
+            }
+            else{
+                radius[0] = r_test[1][0];
+                radius[1] = r_test[1][1];
+            }
         }
     }
 
