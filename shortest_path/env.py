34c74c75ec7157fee8040cf55ba998fe4f8cf546
Gigafortress1 <374377504@qq.com>
2022-06-07 08:53:47 +0800

    rl

---
+++ shortest_path/env.py
@@ -0,0 +1,18 @@
+from turtle import speed
+import numpy as np 
+import pandas
+
+class road():
+    def __init__(self, speed) -> None:
+        self.speed = speed
+
+class env():
+    def __init__(self):
+        pass
+
+    def _build_map(self):
+        pass
+
+    def step(self):
+        
+        pass
